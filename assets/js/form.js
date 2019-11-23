66ab745bb77a1f9662387a961d249dc6a15c9fac
Ben Carlson <bjcarlson42@outlook.com>
2019-11-22 20:30:26 -0500

    Updating the experience and projects paragraphs

---
+++ assets/js/form.js
@@ -0,0 +1,62 @@
+// function validateForm() {
+//     inName = document.getElementById("name").value
+//     inEmail = document.getElementById("email").value
+//     inMessage = document.getElementById("message").value
+//     result = true
+
+//     if (inName == "") {
+//         result = false
+//         document.getElementById('lblName').style.backgroundColor = "lightgray"
+//     }
+//     if (inEmail == "") {
+//         result = false
+//         document.getElementById('lblEmail').style.backgroundColor = "lightgray"
+//     }
+//     if (inMessage == "") {
+//         result = fasle
+//         document.getElementById('lblMessage').style.backgroundColor = "lightgray"
+//     }
+
+//     if (!result) {
+//         document.getElementById("msg").innerHTML = "Please enter valid data in highlighted fields."
+//     }
+
+//     return result
+// }
+
+// function validateForm() {
+//     inName = document.getElementById("uName").value
+//     inPWord = document.getElementById("pWord").value
+//     inDate = document.getElementById("date").value
+//     inCurrency = document.getElementById("currency").value
+//     inNumber = document.getElementById("number").value
+//     result = true
+//     if (inName == "") {
+//         result = false
+//         document.getElementById('lblUsername').style.backgroundColor = "lightgray"
+//     }
+//     if (inPWord == "") {
+//         document.getElementById('lblpassword').style.backgroundColor = "lightgray"
+//         result = false
+//     }
+
+//     if (inDate == "") {
+//         document.getElementById('lblDate').style.backgroundColor = "lightgray"
+//         result = false
+//     }
+
+//     if (inCurrency == "") {
+//         document.getElementById('lblCurrency').style.backgroundColor = "lightgray"
+//         result = false
+//     }
+
+//     if (inNumber == "") {
+//         document.getElementById('lblNumber').style.backgroundColor = "lightgray"
+//         result = false
+//     }
+
+//     if (!result) {
+//         document.getElementById("msg").innerHTML = "Please enter valid data in highlighted fields."
+//     }
+//     return result
+// }
