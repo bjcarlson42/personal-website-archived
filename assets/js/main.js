b2f34504ba0e028f24f89301b54af6aa1c1763e7
Ben Carlson <bjcarlson42@outlook.com>
2019-12-20 11:41:57 -0500

    Style changes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,26 @@
-window.onscroll = function () { myFunction() };
+// window.onscroll = function () { myFunction() };
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
 
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
+// function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
+
+//remove 'Benjamin Carlson' in hamburger menu
+$(function () {
+
+    $(window).bind("resize", function () {
+        console.log($(this).width())
+        if ($(this).width() < 844) {
+            $('#removeMe a').removeClass('notHidden').addClass('hidden')
+        }
+        else {
+            $('#removeMe a').removeClass('hidden').addClass('notHidden')
+        }
+    })
+})
